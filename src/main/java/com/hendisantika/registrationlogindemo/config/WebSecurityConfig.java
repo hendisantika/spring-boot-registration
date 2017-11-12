d4865e6c80035d472d941236b63ee6e79799d603
Hendi Santika <hendisantika@gmail.com>
2017-11-12 10:09:30 +0700

    #15 - Add User Controller

---
+++ src/main/java/com/hendisantika/registrationlogindemo/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.hendisantika.registrationlogindemo.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/17
+ * Time: 10.08
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebSecurityConfig {
+}
