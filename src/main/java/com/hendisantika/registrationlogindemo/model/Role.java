2a2b95f84ddb8d65e9490144b01fd59a6be62b33
Hendi Santika <hendisantika@gmail.com>
2017-11-12 09:29:48 +0700

    #4 - Add Role Model

--- src/main/java/com/hendisantika/registrationlogindemo/model/Role.java
+++ src/main/java/com/hendisantika/registrationlogindemo/model/Role.java
@@ -1,5 +1,8 @@
 package com.hendisantika.registrationlogindemo.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-registration
@@ -10,5 +13,37 @@ package com.hendisantika.registrationlogindemo.model;
  * Time: 09.28
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
 public class Role {
+    private Long id;
+    private String name;
+    private Set<User> users;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
