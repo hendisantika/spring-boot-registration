2e14a0ff6cf6d14799f79071b366d90606b84369
Hendi Santika <hendisantika@gmail.com>
2017-11-12 09:29:32 +0700

    #3 - Add User Model

---
+++ src/main/java/com/hendisantika/registrationlogindemo/model/Role.java
@@ -0,0 +1,14 @@
+package com.hendisantika.registrationlogindemo.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/17
+ * Time: 09.28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Role {
+}
---
+++ src/main/java/com/hendisantika/registrationlogindemo/model/User.java
@@ -0,0 +1,69 @@
+package com.hendisantika.registrationlogindemo.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/17
+ * Time: 09.27
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
