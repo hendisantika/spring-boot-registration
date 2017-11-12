e9caa8ef9d5cc380c7206a05f40eca7334dfa1c6
Hendi Santika <hendisantika@gmail.com>
2017-11-12 09:37:48 +0700

    #10 - Add User Service Impl

---
+++ src/main/java/com/hendisantika/registrationlogindemo/service/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.hendisantika.registrationlogindemo.service;
+
+import com.hendisantika.registrationlogindemo.repository.RoleRepository;
+import com.hendisantika.registrationlogindemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/17
+ * Time: 09.36
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
