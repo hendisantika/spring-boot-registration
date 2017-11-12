8b10158cf43f03e3b75a2d866f7cb21d2ed98017
Hendi Santika <hendisantika@gmail.com>
2017-11-12 09:25:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.registrationlogindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hendisantika/registrationlogindemo/RegistrationLoginDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.registrationlogindemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistrationLoginDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
